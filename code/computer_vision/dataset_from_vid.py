217bb84bbc1341da99874faa6ed1f57242722542
Kamil Kurach <kamilkurach@protonmail.com>
2022-10-10 22:13:54 +0200

    Add fps with multiplier

--- code/computer_vision/dataset_from_vid.py
+++ code/computer_vision/dataset_from_vid.py
@@ -6,17 +6,16 @@ import shutil
 try:
 
     try:
-        path = sys.argv[1]
+        PATH = sys.argv[1]
     except ValueError:
         print("Check if path is valid.")
 
     # skip frames using step size
+    # if step_size = 0 than fps*multiplier
     try:
         STEP_SIZE = int(sys.argv[2])
     except ValueError:
         print("Step size must be int value!")
-
-    cap = cv2.VideoCapture(path)
 
     if os.path.isdir('dataset') == True:
         shutil.rmtree('dataset')
@@ -24,8 +23,16 @@ try:
     if os.path.isdir('dataset') == False:
         os.mkdir('dataset')
 
-    os.chdir('dataset')
+    cap = cv2.VideoCapture(PATH)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    multiplier = 2
     count = 0
+    image_num = 0
+
+    if STEP_SIZE == 0:
+        STEP_SIZE = int(fps*multiplier)
+
+    os.chdir('dataset')
 
     while True:
 
@@ -38,8 +45,9 @@ try:
             break
 
         if count % STEP_SIZE == 0:
-            cv2.imwrite('image_{0}.png'.format(count), frame)
-            print('image_{0}.png'.format(count))
+            cv2.imwrite('image_{0}.png'.format(image_num), frame)
+            print('image_{0}.png'.format(image_num))
+            image_num += 1
         count += 1
 
         if cv2.waitKey(1) == ord('q'):
